80adf194b52dc253b4e9fafee07d0d0d9091a5bb
JJ <j.kubiak164@gmail.com>
2025-07-01 18:20:16 +0200

    added health checks

--- Program.cs
+++ Program.cs
@@ -1,10 +1,12 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddHealthChecks();
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
+app.MapHealthChecks("/health");
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -21,7 +23,7 @@ var summaries = new[]
 
 app.MapGet("/weatherforecast", () =>
 {
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
+    var forecast = Enumerable.Range(1, 5).Select(index =>
         new WeatherForecast
         (
             DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
